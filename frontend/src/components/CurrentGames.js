ad98637da2ba7634f43103806be5d0f948296209
Courtney <cmpeverley@gmail.com>
2018-05-27 22:15:41 -0600

    added current game component

---
+++ frontend/src/components/CurrentGames.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CurrentGames extends Component {
+  render() {
+    return (
+      <div className="CurrentGames">
+      
+      </div>
+    )
+  }
+}
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,13 +5,11 @@ import './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <header className="Home-header">
-          <h1 className="Home-title">Werge</h1>
+      <div className="home">
+        <header className="home-header">
+          <h1 className="home-title">Werge</h1>
         </header>
-        <p className="Home-intro">
-          To get started, edit <code>src/Home.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
