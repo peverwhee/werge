1892fc47c717d8749a8b3e3f87c443753ab18885
Courtney <cmpeverley@gmail.com>
2018-07-16 01:01:10 -0600

    game page updates

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import logo from '../images/werge-logo.png';
+import './Game.css';
 
 export default class Game extends Component {
   render() {
     return(
-        <div></div>
+        <div className="game">
+          <img className='werge-logo' src={logo} />
+        </div>
       );
   }
 }
